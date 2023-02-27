3a1aba477f4b04637542c84b79bda24240bf02d4
SaarthakTuli <saarthaktuli@gmail.com>
2023-02-27 17:15:21 +0530

    data

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,29 @@
 import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "./App.css";
 
 function App() {
-  const [josnData, setJsonData] = useState({});
+  const [josnData, setJsonData] = useState(null);
+
+  const api_url = "https://jsonkeeper.com/b/P2VO";
+
+  // const fetchJson = async () => {
+  //   const res = await fetch(api_url, { mode: "no-cors" });
+  //   const resData = await JSON.parse(res);
+  //   console.log(resData);
+
+  //   return;
+  // };
+
+  // useEffect(() => {
+  //   fetchJson();
+  // }, []);
 
   useEffect(() => {
-    fetch("https://jsonkeeper.com/b/P2VO", {
-      method: "post",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data);
-        setJsonData(data);
-      });
+    axios.get(api_url, { header: "no-cors" }).then((response) => {
+      console.log(response);
+      setJsonData(response.data);
+    });
   }, []);
 
   return (
